1f780d9cb2eb1778ad7c0d9011fd3d548ddcbbc8
Dominique Loulie <dom.loulie@gmail.com>
2023-06-12 09:44:03 +1000

    this is my first

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,21 +1,21 @@
 import React from "react";
 
-function ListingCard() {
+function ListingCard({list}) {
   return (
     <li className="card">
       <div className="image">
-        <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        <span className="price">💲{list.description}</span>
+        <img src={list.image} alt={"description"} />
       </div>
       <div className="details">
         {true ? (
-          <button className="emoji-button favorite active">★</button>
+          <button className="emoji-button favorite active">❤️</button>
         ) : (
-          <button className="emoji-button favorite">☆</button>
+          <button className="emoji-button favorite">🤍</button>
         )}
         <strong>{"description"}</strong>
-        <span> · {"location"}</span>
-        <button className="emoji-button delete">🗑</button>
+        <span> · {list.location}</span>
+        <button className="emoji-button delete">🗑️</button>
       </div>
     </li>
   );
